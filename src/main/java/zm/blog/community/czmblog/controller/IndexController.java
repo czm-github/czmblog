dbc025c6ec307a72e86e1f4be22efab7ec7fafb3
czmgithub9373 <czm-hubit>
2019-12-08 18:28:09 +0800

    paging

--- src/main/java/zm/blog/community/czmblog/controller/IndexController.java
+++ src/main/java/zm/blog/community/czmblog/controller/IndexController.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import zm.blog.community.czmblog.dto.PaginationDTO;
 import zm.blog.community.czmblog.dto.QuestionDTO;
 import zm.blog.community.czmblog.mapper.UserMapper;
 import zm.blog.community.czmblog.model.User;
@@ -25,7 +27,9 @@ public class IndexController {
     /*在服务器重启的时候只要访问首页才能获取到数据库的cookie*/
     @GetMapping("/")
     public String index(HttpServletRequest request,
-                        Model model){
+                        Model model,
+                        @RequestParam(name = "page",defaultValue = "1") Integer page,
+                        @RequestParam(name = "size",defaultValue = "5") Integer size){
         Cookie[] cookies = request.getCookies();
         /*因为这个cookies如果被删除掉了就会报null异常，所以需要添加一个判断让用户重新点击登录*/
         if(cookies != null && cookies.length != 0){
@@ -40,8 +44,8 @@ public class IndexController {
                 }
             }
         }
-        List<QuestionDTO> questionDTOList = questionService.list();
-        model.addAttribute("questions",questionDTOList);
+        PaginationDTO paginaction = questionService.list(page,size);
+        model.addAttribute("paginaction",paginaction);
         return "index";
     }
 
--- src/main/java/zm/blog/community/czmblog/service/QuestionService.java
+++ src/main/java/zm/blog/community/czmblog/service/QuestionService.java
@@ -3,6 +3,8 @@ package zm.blog.community.czmblog.service;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import sun.security.krb5.internal.PAData;
+import zm.blog.community.czmblog.dto.PaginationDTO;
 import zm.blog.community.czmblog.dto.QuestionDTO;
 import zm.blog.community.czmblog.mapper.QuestionMapper;
 import zm.blog.community.czmblog.mapper.UserMapper;
@@ -25,9 +27,21 @@ public class QuestionService {
     private UserMapper userMapper;
 
 
-    public List<QuestionDTO> list() {
-        List<Question> questions = questionMapper.list();
+    public PaginationDTO list(Integer page, Integer size) {
+        PaginationDTO paginationDTO = new PaginationDTO();
+        Integer totalCount = questionMapper.count();
+        paginationDTO.setPagination(totalCount,page,size);
+        if(page < 1){
+            page = 1;
+        }else  if(page > paginationDTO.getTotalPage()){
+            page = paginationDTO.getTotalPage();
+        }
+
+        //偏移量中的第一个参数，表示从第几条数据开始，第二个参数为size表示展示的数据为固定5
+        Integer offset = size * (page - 1);
+        List<Question> questions = questionMapper.list(offset,size);
         List<QuestionDTO> questionDTOList = new ArrayList<>();
+
         for(Question question : questions){
             User user = userMapper.findById(question.getCreator());
             QuestionDTO questionDTO = new QuestionDTO();
@@ -35,6 +49,7 @@ public class QuestionService {
             questionDTO.setUser(user);
             questionDTOList.add(questionDTO);
         }
-        return questionDTOList;
+        paginationDTO.setQuestionDTOS(questionDTOList);
+        return paginationDTO;
     }
 }
